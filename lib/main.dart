b183743a06e621be56b37cd037eb97ac4cac3e8b
EVER_DANIEL_PEREYRA_CASTILLO <a.22308051281282@cbtis128.edu.mx>
2025-02-26 20:46:44 +0000

    iconos

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,110 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
+  const MisIconos({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Ever Daniel Pereyra Castillo'),
+          titleTextStyle:
+              const TextStyle(color: Color(0xff005e03), fontSize: 20),
+          centerTitle: true,
+          backgroundColor: const Color(0xffbcffbe),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Ever Daniel Pereyra Castillo Mat: 22308051281282\n\n\n",
+                style: TextStyle(color: Color(0xff005e03), fontSize: 18),
+              ),
+              // Primera fila de iconos
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconWithLabel(
+                    icon: Icons.access_alarm,
+                    label: 'Tiempo',
+                    color: Color(0xffdbc500),
+                  ),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithLabel(
+                    icon: Icons.thumb_down_alt_sharp,
+                    label: 'No me gusta',
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  IconWithLabel(
+                    icon: Icons.list,
+                    label: 'OPC',
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+              SizedBox(height: 20), // Espacio entre las filas
+              // Segunda fila de iconos
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconWithLabel(
+                    icon: Icons.museum,
+                    label: 'Home',
+                    color: Color(0xff000000),
+                  ),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithLabel(
+                    icon: Icons.adb_sharp,
+                    label: 'Android',
+                    color: Colors.green,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  IconWithLabel(
+                    icon: Icons.apple,
+                    label: 'Apple',
+                    color: Color(0xff979797),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+// Widget personalizado para un icono con subtítulo
+class IconWithLabel extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final Color color;
+
+  const IconWithLabel({
+    required this.icon,
+    required this.label,
+    required this.color,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: color),
+        SizedBox(height: 8), // Espacio entre el icono y el texto
+        Text(label, style: TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
